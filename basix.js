9e9dc6fa3d1019cf000edc159f903b7aa4a7511e
Sanjeev Chadeesingh <leolal1984@icloud.com>
2020-06-11 10:36:45 -0400

    project js initial engagement

---
+++ basix.js
@@ -0,0 +1,7 @@
+console.log('6/11, 10:00AM. Starting with the Jonas beginning.');
+console.log('Hiyah There!');
+var firstName = 'Sanjeev';
+var lastName = 'Chadeesingh';
+var age  = 36;
+var fullAge = true;
+var job = 'coder';
