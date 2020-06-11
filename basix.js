493e6843a5d481785c0de27327c456b4ffd000e2
Sanjeev Chadeesingh <leolal1984@icloud.com>
2020-06-11 14:14:02 -0400

    basics completed

--- basix.js
+++ basix.js
@@ -1,3 +1,4 @@
+//BASIC VARIABLES: DECLARATIONS, TYPES
 console.log('6/11, 10:00AM. Starting with the Jonas beginning.');
 
 console.log('Hiyah There!');
@@ -66,3 +67,106 @@ console.log(x);
 var y = 3;
 y = y + 3;
 y += 3;
+
+//CODING CHALLENGE
+var markWeight, markHeight, johnWeight, johnHeight;
+markWeight = 100;
+markHeight = 2.03;
+johnWeight = 78;
+johnHeight = 1.72;
+var markBMI = markWeight / (markHeight * markHeight);
+var johnBMI = johnWeight / (johnHeight * johnHeight);
+var higherBMI = markBMI > johnBMI;
+console.log('John: ' + johnBMI, 'Mark: ' + markBMI);
+console.log('Is Mark\'s BMI higher than John\'s?' + ' ' + higherBMI);
+if(markBMI > johnBMI) {
+    console.log('Mark\'s BMI is higher.')
+} else {
+    console.log('John\'s BMI is higher.');
+}
+
+//IF ELSE STATEMENTS
+var firstName = 'Sanjeev';
+var marriageStatus = 'single';
+if (marriageStatus === 'married') {
+    console.log(firstName + ' is married.');
+} else {
+    console.log(firstName + ' will hopefully marry soon.');
+}
+
+var isMarried = true;
+if (isMarried) {
+    console.log(firstName + ' is married.');
+} else {
+    console.log(firstName + ' will hopefully marry soon.');
+}
+
+//BOOLEAN LOGIC
+var firstName = 'Sanjeev';
+var age = 36 ;
+if(age < 13) {
+    console.log(firstName + ' is a boy.')
+} else if (age >= 13 && age < 20) {
+    console.log(firstName + ' is a teenager');
+} else {
+    console.log(firstName + ' is a man.');
+}
+
+//TERNARY OPERATOR
+var firstName = 'Sanjeev';
+var age = 36;
+age >=21 ? console.log('Have a beer!') : console.log('Have a juice.');
+var drink = age >= 18 ? 'beer' : 'juice';
+console.log(firstName + ' ' + drink);
+
+//SWITCH STATEMENT
+var job = 'fighter';
+switch (job) {
+    case 'coder':
+    case 'developer':
+        console.log(firstName + ' is a coder.');
+        break;
+    case 'driver':
+        console.log(firstName + ' is a driver.');
+        break;
+    case 'designer':
+        console.log(firstName + ' is a designer.');
+        break;
+    default:
+        console.log(firstName + ' is something else entirely.');
+}
+
+age = 36;
+
+switch (true) {
+    case age < 13:
+        console.log(firstName + ' is a boy.');
+        break;
+    case age >=13 && age < 20:
+        console.log(firstName + ' is a teenager.');
+        break;
+    default:
+        console.log(firstName + ' is a man.');
+}
+
+//TRUTHY AND FALSY VALUES
+var height;
+height = 0; 
+if (height) {
+    console.log('variable is defined');
+} else {
+    console.log('variable is undefined');
+}
+
+//CODING CHALLENGE 2
+var johnAverage = (89 + 120 + 103) / 3;
+var mikeAverage = (116 + 94 + 123) / 3;
+var maryAverage = (97 + 134 + 105) / 3;
+console.log(johnAverage, mikeAverage, maryAverage);
+if (johnAverage > mikeAverage && johnAverage > maryAverage) {
+    console.log('John\'s team has the highest average, with ' + johnAverage + ' points.');
+} else if (mikeAverage > johnAverage && mikeAverage > maryAverage) {
+    console.log('Mike\'s team has the highest average, with ' + mikeAverage + ' points.');
+} else if (maryAverage > johnAverage && maryAverage > mikeAverage) {
+    console.log('Mary\'s team has the highest average, with ' + maryAverage + ' points.');
+}
