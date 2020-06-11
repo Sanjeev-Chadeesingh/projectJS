974d1a9cd71246a0f266c5143a24166bf757857e
Sanjeev Chadeesingh <leolal1984@icloud.com>
2020-06-11 11:44:40 -0400

    basic operators, math and logic and typeof added

--- basix.js
+++ basix.js
@@ -1,7 +1,47 @@
 console.log('6/11, 10:00AM. Starting with the Jonas beginning.');
+
 console.log('Hiyah There!');
+
 var firstName = 'Sanjeev';
+
 var lastName = 'Chadeesingh';
+
 var age  = 36;
+
 var fullAge = true;
+
 var job = 'coder';
+
+//VARIABLE MUTATION AND TYPE COERCION
+var firstName = 'Sanjeev';
+
+var age = 36;
+
+var job, isMarried;
+
+job = 'coder';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married?' + ' ' + isMarried);
+
+//alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married?' + ' ' + isMarried);
+
+//var lastName = prompt('What is his last name?');
+console.log(firstName + lastName);
+//alert(firstName + ' ' + lastName);
+
+//BASIC OPERATORS
+    //math
+var now, ageSanjeev, ageShanti;
+now = 2020;
+ageSanjeev = 36;
+ageShanti = 34;
+var yearSanjeev = now - ageSanjeev;
+console.log(ageSanjeev);
+console.log(ageSanjeev * 2);
+    //logic
+var sanjeevOlder = ageSanjeev > ageShanti;
+console.log(sanjeevOlder);
+    //typeof
+console.log(typeof sanjeevOlder);
+console.log(typeof ageSanjeev)
